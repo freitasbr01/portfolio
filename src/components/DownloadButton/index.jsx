df6933afd52ebfbdef15fd1eafb1bb7844e42512
Alan <freitasbr01@gmail.com>
2024-06-28 19:53:55 -0300

    updated

--- src/components/DownloadButton/index.jsx
+++ src/components/DownloadButton/index.jsx
@@ -3,8 +3,8 @@ import { LuDownload } from "react-icons/lu";
 
 
 export function DownloadButton({ ...rest }) {
-  // const downloadUrl = 'https://drive.google.com/uc?export=download&id=1ON1nbaL90oTUB0q2hJfFvn3uqw4Wrx4J';
   const downloadUrl = 'https://raw.githubusercontent.com/freitasbr01/portfolio/main/src/assets/file/Alan%20Pereira%20de%20Freitas.pdf';
+  
   return (
     <Container href={downloadUrl} download="Curriculo_AlanFreitas.pdf" target="_blank">
       CURRÍCULO <LuDownload />
